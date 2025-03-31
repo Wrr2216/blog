b1573be85fbf3c4b6f6e2afc1456c2d1546df0a7
Logan Miller <logan@millercyber.tech>
2025-03-31 16:48:41 -0500

    Updates

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "standalone",   // <--- IMPORTANT FOR NETLIFY SSR
+
   images: {
     remotePatterns: [
       {
         protocol: "https",
         hostname: "cdn.hashnode.com",
-        port: "",
       },
     ],
   },
